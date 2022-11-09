451eadf635b522a3be45634bb803e75a526cecd6
밤밤 <jiiyo0@naver.com>
2022-11-09 16:38:43 +0900

    221109

--- JavaScript/vendingmachine/src/js/index.js
+++ JavaScript/vendingmachine/src/js/index.js
@@ -1,5 +1,10 @@
 import ColaGenerator from "./components/colaGenerator.js";
+import Vendingmachine from "./components/vendingmachine.js";
 
 const colaGenerator = new ColaGenerator();
+const vendingmachine = new Vendingmachine();
 
-colaGenerator.setup();
+//await
+
+await colaGenerator.setup();
+vendingmachine.setup();
---
+++ Node/003.js
@@ -0,0 +1,5 @@
+const 이름 = "호준";
+const 나이 = 10;
+
+module.export.이름 = 이름;
+module.export.나이 = 나이;
