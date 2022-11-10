8841f3419130dd839c32efd64e63a28dd4bd7cc8
밤밤 <jiiyo0@naver.com>
2022-11-11 01:41:23 +0900

    221110

--- Node/003.js
+++ Node/003.js
@@ -1,5 +1,5 @@
-const 이름 = "호준";
+const 이름 = '호준';
 const 나이 = 10;
 
-module.export.이름 = 이름;
-module.export.나이 = 나이;
+module.exports.이름 = 이름;
+module.exports.나이 = 나이;
---
+++ React/022/my-app/components/ResultCont.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ResultCont() {
+   return <div>ResultCont</div>;
+}
