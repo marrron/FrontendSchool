dcd887f2f59aa3e37beed0aa9d0d478d1d372d12
밤밤 <112460285+marrron@users.noreply.github.com>
2022-11-24 16:16:39 +0900

    Delete React/프로젝트1 directory

--- React/프로젝트1/my-app/src/components/Card/ProductImage/ProductImage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ProductImage({ thumbnailImg }) {
-	// console.log(thumbnailImg)
-	const url = "https://test.api.weniv.co.kr/" + thumbnailImg;
-	return <img src={url} />;
-}
--- React/프로젝트1/my-app/src/components/Card/ProductPrice/ProductPrice.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ProductPrice({price}) {
-  return (
-    <div>{price}</div>
-  )
-}
